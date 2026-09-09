526e14550a4b58b077316d7c5d4edcfd3ec34173
bhaviksaglani <bhaviksaglani1998@gmail.com>
2026-09-09 05:30:39 +0530

    Added photos and videos with correct paths

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Forgiveness Button Logic
+// Forgiveness Button Logic with animation
 const btn = document.getElementById("forgiveBtn");
 btn.addEventListener("click", () => {
   btn.innerText = "Yes 💖";
+  btn.style.background = "linear-gradient(135deg, #a1ffce, #faffd1)";
+  btn.style.color = "#333";
+  btn.style.transform = "scale(1.2)";
+  setTimeout(() => {
+    btn.style.transform = "scale(1)";
+  }, 500);
 });
